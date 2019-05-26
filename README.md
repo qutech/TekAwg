04cb73aa1e978bdd38a00e3f79db659d6e7f1491
Simon Humpohl (Wet Fridge) <simon.humpohl@rwth-aachen.de>
2019-05-26 15:36:55 +0200

    Rename to tek_awg

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Python driver for Tektronix AWG 5000/7000 series.
 
 This fork uses `pyvisa` for communication.
+
+Forked from https://github.com/dahlend/TekAwg/
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
-setup(name='TekAwg',
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta"
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Topic :: System :: Hardware :: Hardware Drivers",
+]
+
+setup(name='tek_awg',
       version='0.1',
-      description='Tektronix AWG ethernet interface for python',
-      author='Dar Dahlen',
-      author_email='dardahlen@gmail.com',
-      url='https://github.com/dahlend/TekAwg/',
-      py_modules=['TekAwg'],
+      description='Tektronix AWG interface for python',
+      author='Simon Humpohl',
+      author_email='simon.humpohl@rwth-aachen.de',
+      url='https://github.com/qutech/TekAwg/',
+      py_modules=['tek_awg'],
       install_requires=['numpy', 'pyvisa']
-      )
+)
